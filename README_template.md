dffcb1edc414d6b0feb452abc3b79d52b991d985
qayoommunawar <maharsahb058@gmail.com>
2025-06-03 12:57:56 +0500

    first commit

---
+++ README_template.md
@@ -0,0 +1,59 @@
+<!-- Please update value in the {}  -->
+
+<h1 align="center">{Multi Step Register Form} | devChallenges</h1>
+
+<div align="center">
+   Solution for a challenge <a href="https://devchallenges.io/challenge/multi-step-register-form" target="_blank">Multi-step Register Form</a> from <a href="http://devchallenges.io" target="_blank">devChallenges.io</a>.
+</div>
+
+<div align="center">
+  <h3>
+    <a href="{https://your-demo-link.your-domain}">
+      Demo
+    </a>
+    <span> | </span>
+    <a href="{https://your-url-to-the-solution}">
+      Solution
+    </a>
+    <span> | </span>
+    <a href="https://devchallenges.io/challenge/multi-step-register-form">
+      Challenge
+    </a>
+  </h3>
+</div>
+
+<!-- OVERVIEW -->
+
+## Overview
+
+![screenshot](screenshot.png)
+
+
+### What I learned
+
+- I practiced DOM manipulation how to connect different events. Getting relevant click event result was an acheivment.
+
+
+### Built with
+
+<!-- This section should list any major frameworks that you built your project using. Here are a few examples.-->
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Vanilla Js
+
+## Features
+
+- Toggle between steps 
+- Real time input and data change in final step from previous steps
+- Fully Responsive
+
+## Continue Development
+
+- Adding top left border as per in the design
+- Form validation for all the three steps
+- Giving ech step a smooth animation
+- Success message to be added
+
+
---
+++ script.js
@@ -0,0 +1,118 @@
+const btnFooter = document.querySelectorAll('.btn-circle');
+const btnFooter1 = document.querySelector('.btn_circle1');
+const steps = document.querySelectorAll('.steps');
+const step1 = document.querySelector('.step_1');
+const btnTopic = document.querySelectorAll('.btn_topic');
+const btnContinue = document.querySelectorAll('.btn_continue');
+const form = document.querySelector('form');
+const inputName = document.querySelector('#name');
+const inputEmail = document.querySelector('#email');
+const nameFinal = document.querySelector('.summary_name');
+const emailFinal = document.querySelector('.summary_email');
+const topics = document.querySelectorAll('.topics')
+const btnReset = document.querySelector('.btn_reset');
+
+
+// First Step : Enabling Next & Previous Buttons
+
+btnFooter.forEach((button,index) => {
+    button.addEventListener('click', ()=>{
+
+        // Removing active Class from the buttons
+        btnFooter.forEach((btn) =>{btn.classList.remove('active_footer')});
+
+        // Adding active class to the clicked button
+        button.classList.add('active_footer');
+
+        //Hide all the steps
+        steps.forEach((step) => step.classList.add('visually_hidden'));
+
+        // showing the relevant step
+        steps[index].classList.remove('visually_hidden');
+    });
+
+});
+
+
+// Second Step: Selecting The Topic Of Step Two
+
+btnTopic.forEach((btn) => {
+    btn.addEventListener('click', ()=>{
+        // applying active class
+        btn.classList.toggle('active_topic');
+    })
+});
+
+
+// Third Step: Continue Buttons for next step only
+
+btnContinue.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      if (index < steps.length - 1) {
+        // Hide current step
+        steps[index].classList.add('visually_hidden');
+        // Show next step
+        steps[index + 1].classList.remove('visually_hidden');
+  
+        // Update footer buttons
+        btnFooter.forEach(btn => btn.classList.remove('active_footer'));
+        btnFooter[index + 1].classList.add('active_footer');
+      }
+    });
+});
+
+// Fourth Step: updating first step indput data to third step in real time
+
+// Updating Name
+inputName.addEventListener('input', ()=>{
+    nameFinal.textContent = inputName.value;
+});
+
+// Updating email
+inputEmail.addEventListener('input', ()=>{
+    emailFinal.textContent = inputEmail.value;
+});
+
+
+// Fifth Step: updatind data from step two to third step
+
+btnTopic.forEach((btn,index) => {
+    btn.addEventListener('click', ()=>{
+        topics[index].textContent = btn.textContent;
+    })
+});
+
+
+// Sixth Step:  resetting the whole steps
+
+btnReset.addEventListener('click', () => {
+    // 1.Activating step no.1
+
+    //Hide all the steps
+    steps.forEach((step) => step.classList.add('visually_hidden'));
+
+    // showing the relevant step
+    step1.classList.remove('visually_hidden');
+
+    // Removing active Class from the buttons
+    btnFooter.forEach((btn) =>{btn.classList.remove('active_footer')});
+
+    btnFooter1.classList.add('active_footer');
+
+    // 2.Removing data from first step's inputs
+    inputName.value = "";
+    inputEmail.value = '';
+
+    // 3.Removing active class from topic buttons
+
+    btnTopic.forEach((btn) => {
+        // applying active class
+        btn.classList.remove('active_topic');
+    });
+
+})
+
+// Seven Step: Emptying inputs on relaod
+window.onload = function () {
+    form.reset();
+}
